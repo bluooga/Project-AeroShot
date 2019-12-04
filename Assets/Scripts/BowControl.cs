b8f9397cd54150d0e8e5fdc0b192b6760d139dfb
bluooga <fabiandcool17807@gmail.com>
2019-12-04 00:39:47 -0800

    these are the project files

---
+++ Assets/Scripts/BowControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowControl : MonoBehaviour
+{
+
+    public GameObject ArrowPrefab;
+    public Vector3 objectSpawnOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireArrow();
+        }
+    }
+
+    void FireArrow()
+    {
+        Instantiate(ArrowPrefab, transform.forward + objectSpawnOffset, Quaternion.Euler(0, transform.rotation.y, 0));
+    }
+
+}
